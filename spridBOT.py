426daab4dcd008afbf138d76744d378634a36249
samuelpriddy <srpriddy@ad.unc.edu>
2023-01-19 21:24:54 -0500

    Plays 1.e4 if given white pieces

--- spridBOT.py
+++ spridBOT.py
@@ -1,13 +1,16 @@
 import threading
 import berserk
 
+# Useful Websites
+# https://berserk.readthedocs.io/en/master/usage.html
+# https://lichess.org/api#tag/Bot
+
 token = 'lip_e8lT0QKAbYWOx2RQc7Py'
 session = berserk.TokenSession(token)
 client = berserk.Client(session=session)
 
 class Game(threading.Thread):
-
-    def __init__(self, client, game_id, **kwargs):
+    def __init__(self, client, game_id, color, **kwargs):
 
         super().__init__(**kwargs)
 
@@ -19,6 +22,9 @@ class Game(threading.Thread):
 
         self.current_state = next(self.stream)
 
+        if(color == 'white'):
+            client.bots.make_move(self.game_id, 'e2e4')
+
 
     def run(self):
 
@@ -28,10 +34,10 @@ class Game(threading.Thread):
 
                 self.handle_state_change(event)
 
-
     def handle_state_change(self, game_state):
+        print(self.game_id)
 
-        client.bots.abort_game(self.game_id)
+        
 
 for event in client.bots.stream_incoming_events():
 
@@ -39,6 +45,6 @@ for event in client.bots.stream_incoming_events():
         client.bots.accept_challenge(event['challenge']['id'])
 
     elif event['type'] == 'gameStart':
-        game = Game(client, game_id = event['game']['gameId'])
+        game = Game(client, game_id = event['game']['gameId'], color = event['game']['color'])
 
         game.start()
