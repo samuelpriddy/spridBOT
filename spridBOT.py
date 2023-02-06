b1b1f4864552b771edc14dcf2a88ce5a411d527b
samuelpriddy <srpriddy@ad.unc.edu>
2023-02-06 17:33:27 -0500

    Greedy Algo

--- spridBOT.py
+++ spridBOT.py
@@ -1,29 +1,51 @@
 import threading
 import berserk
 import chess
-import random
 
 # Useful Websites
 # https://berserk.readthedocs.io/en/master/usage.html
 # https://lichess.org/api#tag/Bot
+## https://python-chess.readthedocs.io/en/latest/
 
 token = 'lip_e8lT0QKAbYWOx2RQc7Py'
 session = berserk.TokenSession(token)
 client = berserk.Client(session=session)
 
-def randomMoveGenerator(board):
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-    numbers = ['1', '2', '3', '4', '5', '6', '7', '8']
-    while(True):
-        l1 = letters[random.randint(0,7)]
-        l2 = letters[random.randint(0,7)]
-        n1 = numbers[random.randint(0,7)]
-        n2 = numbers[random.randint(0,7)]
-        try:
-            if chess.Move.from_uci(l1+n1+l2+n2) in board.legal_moves:
-                return l1+n1+l2+n2
-        except:
-            pass
+def findPointDifferential(board):
+    whiteSum = 0
+    blackSum = 0
+    whiteSum += len(list(board.pieces(1, True)))
+    whiteSum += len(list(board.pieces(2, True))) * 3
+    whiteSum += len(list(board.pieces(3, True))) * 3
+    whiteSum += len(list(board.pieces(4, True))) * 5
+    whiteSum += len(list(board.pieces(5, True))) * 9
+    blackSum += len(list(board.pieces(1, False)))
+    blackSum += len(list(board.pieces(2, False))) * 3
+    blackSum += len(list(board.pieces(3, False))) * 3
+    blackSum += len(list(board.pieces(4, False))) * 5
+    blackSum += len(list(board.pieces(5, False))) * 9
+    return whiteSum - blackSum
+
+def findBestMove(board, color):
+    bestMove = None
+    bestMovePointDiff = None
+
+    for move in board.legal_moves:
+        board.push(move)
+        pointDiff = findPointDifferential(board)
+        if(bestMove == None): 
+            bestMove = move
+            bestMovePointDiff = pointDiff
+        elif(color == 'white' and pointDiff > bestMovePointDiff):
+            bestMove = move
+            bestMovePointDiff = pointDiff
+        elif(color == 'black' and pointDiff < bestMovePointDiff):
+            bestMove = move
+            bestMovePointDiff = pointDiff
+        board.pop()
+
+    return bestMove
+        
 
 
 class Game(threading.Thread):
@@ -42,11 +64,13 @@ class Game(threading.Thread):
 
         self.current_state = next(self.stream)
 
+        self.color = color
+
         if(color == 'white'):
-            move = randomMoveGenerator(self.board)
+            move = findBestMove(self.board, self.color)
             client.bots.make_move(game_id, move)
-            self.board.push_uci(move)
-            self.lastMove = move
+            self.board.push(move)
+            self.lastMove = move.uci()
 
     def run(self):
 
@@ -58,17 +82,13 @@ class Game(threading.Thread):
 
     def handle_state_change(self, game_state):
         a = game_state['moves'].split(' ')
-
+        print(a)
         if(a[len(a)-1] != self.lastMove):
             self.board.push_uci(a[len(a)-1])
-            move = randomMoveGenerator(self.board)
+            move = findBestMove(self.board, self.color)
             client.bots.make_move(self.game_id, move)
-            self.board.push_uci(move)
-            self.lastMove = move
-
-        
-
-        
+            self.board.push(move)
+            self.lastMove = move.uci()
 
 for event in client.bots.stream_incoming_events():
 
